35060d0a059bc363fca09976d20c707cf6cb504f
kerentang <kerentang@172-13-8-212.lightspeed.wchtks.sbcglobal.net>
2017-04-18 13:21:18 +0800

    添加和删除有点问题

---
+++ ifewebjs09/script.js
@@ -0,0 +1,136 @@
+'use strict'
+//获取dom节点-id
+function $id (id) {
+	return document.getElementById(id);
+}
+
+//获取各节点
+var myroot=$id('root');//获取dom树的根节点
+var myinput=$id('input-text');//获取输入框的节点，输入框的值为myinput.value;
+var inputtext=myinput.value;
+var timer=null;//初始化定时器
+var nodearr=[];//定义一个空数组用来保存节点
+
+//遍历操作-广度优先／深度优先；
+//广度优先-递归;
+function mylfre(node){
+      nodearr.push(node);
+      var arr = [];
+      arr.push(node);  //专门用来取第一项子节点,因为arr.shift()和arr.pop()会更改原数组，所以新建一个数组专门
+      while(arr.length != 0) {
+          var temp = arr.shift();
+          var n = temp.childNodes.length;
+          for(var i = 0;i < n;i++) {
+            if (temp.childNodes[i].nodeType == "1") {
+                nodearr.push(temp.childNodes[i]);
+                arr.push(temp.childNodes[i]);
+            }
+        }
+    } 
+}
+
+
+//深度优先-递归;:children[0]==textNode,注意！所以不能用firsrElementChild,要用children[1],
+//后面要查看第一项子节点的内容所以还是从0开始。。。
+function myffre (node) {
+	if(node!==null){
+    	nodearr.push(node);
+    	if(node.children.length>0){
+    		var child=node.children[0];
+            for(var i=0;i<node.children.length;i++){
+                myffre(node.children[i]);
+    	    }
+    	}
+    }
+    return nodearr;
+}
+
+//创建新元素
+function createele (inputtext) {
+    var ele=document.createElement('div');
+    var elep=document.createElement('p');
+    var myinputtext=docunment.createTextNode(inputtext);
+    elep.appendChild(myinputtext);
+    ele.appendChild(elep);
+    ele.setAttribute('class','box');
+    return ele;
+}
+
+//定位元素位置
+var mynode;
+function getnode (e) {
+    var e=e || window.e;
+    var target=e.target || e.srcElement;
+    if(target.nodeName.toLowerCase()=== 'div' && target.getAttribute('class')=='box'){
+        target.style.backgroundColor='#00f';
+        target.style.color='#f00';
+        mynode=target;
+        return mynode;
+    }else{alert('选择节点无效！')}
+}
+
+//处理添加函数
+function addele () {
+   var mynodeadd=getnode(e);
+   var myele=createele(inputtext);
+   return mynodeadd.appendChild(myele);
+}
+//删除元素
+function delele () {
+    var mynodedel=getnode(e);
+    var myparent=mynodedel.parentNode;
+    myparent.innerHTML='';
+}
+
+//change_style()给遍历的节点添加额外的样式背景颜色变成黄色
+function change_style () {
+	var i=0;
+    timer=setInterval(function() {
+		if(i>nodearr.length-1){
+			clearInterval(timer);
+			nodearr[nodearr.length-1].style.backgroundColor='#0ff';
+			return;
+		}else if(i<nodearr.length){
+		    if(i>0){
+		       nodearr[i-1].style.backgroundColor='#0ff';
+		    }
+			nodearr[i].style.backgroundColor='#ff0';
+		}
+		i++;
+	},500);
+}
+
+//清除添加的样式
+function clearstyle(){
+	clearInterval(timer);//取消定时执行的timer,以便能随时开启另外一种遍历而不会存在上一次的timer的干扰
+    for(var i=0;i<nodearr.length;i++){
+    	nodearr[i].style.backgroundColor='#fff';
+    }
+    nodearr=[];//清除上一次点击的样式改变
+}
+
+//将各个阶段的事件写在一起方便监听
+function init(e){
+	clearstyle();
+    var e=e || window.e,target=e.target || e.srcElement;
+    switch(target.id){
+        case 'lf-re':
+            mylfre(myroot)
+            break;
+        case 'ff-re':
+            myffre(myroot)
+            break;
+    }
+	change_style();
+}
+
+
+//各个btn的监听事件:
+window.onload=function(){
+	$id('lf-re').addEventListener('click',init);
+	$id('ff-re').addEventListener('click',init);
+    $id('clear').addEventListener('click',clearstyle);
+    $id('root').addEventListener('click',getnode);
+    $id('add-ele').addEventListener('click',addele);
+    $id('del-ele').addEventListener('click',delele);
+}
