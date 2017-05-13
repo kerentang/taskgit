92be9cac496a4b61c5603787843805c2a650baea
kerentang <kerentang@kerentangdeMacBook-Air.local>
2017-05-13 22:29:58 +0800

    终于理解了event

---
+++ ifenuomivue1.js
@@ -0,0 +1,42 @@
+function Observer(data){
+    this.data=data;
+    this.walk(data);
+}
+var pro=Observer.prototype;
+		pro.walk=function(obj){
+            var val;
+            for(var key in obj){
+                if(obj.hasOwnProperty(key)){
+                    val=obj[key];
+                    if(typeof val ==='object'){
+                        new Observer(val);
+                    } 
+                    this.convert(key,val);
+                }
+            }
+		};
+
+		pro.convert=function(key,val){
+		    Object.defineProperty(this.data,key,{
+		        enumerable:true,
+		        configurable:true,
+		        get:function(){
+		        	console.log('你访问了'+key);
+		        	return val;
+		        },
+		        set:function(newVal){
+		            console.log('你设置了'+key);
+		            console.log('新的'+key+'='+newVal)
+		            if(newVal===val){
+		                return;
+		            }
+		            val=newVal;
+		        }
+		    })
+};
+		var app1=new Observer({
+                name:'新的name',
+                age:'24'
+		});
+		app1.data.name='word tiana';
+		
