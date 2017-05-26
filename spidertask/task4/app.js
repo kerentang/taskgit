6b94f108ac001f35c23614d76e38039cab2ce302
kerentang <kerentang@172-13-1-64.lightspeed.mssnks.sbcglobal.net>
2017-05-26 17:33:59 +0800

    nunjunks

--- spidertask/task4/app.js
+++ spidertask/task4/app.js
@@ -2,25 +2,47 @@
 // import _ from 'lodash';
 // require('./mystyle.css');引入css文件打包
 // require('./youstyle.css');
-// var moment = require('moment');
-// console.log(moment().format());
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const router = require('koa-router')();
+const path = require('path');
+// const stafiles = require('koa-static');
+// var moment = require('moment');
+// console.log(moment().format());
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 // 导入controller middleware:
 const controller = require('./controller');
+const isProduction = process.env.NODE_ENV === 'production';
+const templating = require('./templating');
 // log request URL:
 app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-    await next();
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+  var
+    start = new Date().getTime(),
+    execTime;
+  await next();
+  execTime = new Date().getTime() - start;
+  ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
+// app
+// static file support:
+if (!isProduction) {
+  let staticFiles = require('./static-files');
+  app.use(staticFiles('/static/', __dirname + '/static'));
+}
+
 app.use(bodyParser());
+
+app.use(templating('views', {
+  noCache: !isProduction,
+  watch: !isProduction
+}));
+
 // app.use(router.routes());
 // add router middleware:
 app.use(controller());
-app.listen(3000);
-console.log('app started at port 3000...');
+app.listen(8080);
+console.log('app started at port 8080...');
--- spidertask/task4/controller.js
+++ spidertask/task4/controller.js
@@ -1,36 +1,39 @@
 const fs = require('fs');
-function addMapping(router, mapping) {
-    for (var url in mapping) {
-        if (url.startsWith('GET ')) {
-            var path = url.substring(4);
-            router.get(path, mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-        } else if (url.startsWith('POST ')) {
-            var path = url.substring(5);
-            router.post(path, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-        } else {
-            console.log(`invalid URL: ${url}`);
-        }
+const path = require('path');
+function addMapping (router, mapping) {
+  for (var url in mapping) {
+    if (url.startsWith('GET ')) {
+      let path = url.substring(4);
+      router.get(path, mapping[url]);
+      console.log(`register URL mapping: GET ${path}`);
+    } else if (url.startsWith('POST ')) {
+      let path = url.substring(5);
+      router.post(path, mapping[url]);
+      console.log(`register URL mapping: POST ${path}`);
+    } else {
+      console.log(`invalid URL: ${url}`);
     }
+  }
 }
 
-function addControllers(router) {
-    var files = fs.readdirSync(__dirname + '/controllers');
-    var js_files = files.filter((f) => {
-        return f.endsWith('.js');
-    });
+function addControllers (router) {
+  let filePath = path.join(__dirname, '/controllers');
+  var files = fs.readdirSync(filePath);
+  var jsFiles = files.filter((f) => {
+    return f.endsWith('.js');
+  });
 
-    for (var f of js_files) {
-        console.log(`process controller: ${f}...`);
-        let mapping = require(__dirname + '/controllers/' + f);
-        addMapping(router, mapping);
-    }
+  for (var f of jsFiles) {
+    console.log(`process controller: ${f}...`);
+    let jsfilesPath = path.join(__dirname, '/controllers/', f);
+    let mapping = require(jsfilesPath);
+    addMapping(router, mapping);
+  }
 }
 module.exports = function (dir) {
-    let
-        controllers_dir = dir || 'controllers', // 如果不传参数，扫描目录默认为'controllers'
-        router = require('koa-router')();
-    addControllers(router, controllers_dir);
-    return router.routes();
+  var
+    controllersDir = dir || 'controllers', // 如果不传参数，扫描目录默认为'controllers'
+    router = require('koa-router')();
+  addControllers(router, controllersDir);
+  return router.routes();
 };
