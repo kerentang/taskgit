54a981dccfb6fb3cf52fbcff0c95aee0b65d24fc
kerentang <kerentang@172-13-12-135.lightspeed.wchtks.sbcglobal.net>
2017-06-05 13:31:52 +0800

    douban api and vue

--- ifewebjs09/script.js
+++ ifewebjs09/script.js
@@ -5,15 +5,15 @@ function $id (id) {
 }
 
 //获取各节点
-var myroot=$id('root');//获取dom树的根节点
-var myinput=$id('input-text');//获取输入框的节点，输入框的值为myinput.value;
-var inputtext=myinput.value;
-var timer=null;//初始化定时器
-var nodearr=[];//定义一个空数组用来保存节点
+var myroot = $id('root');//获取dom树的根节点
+var myinput = $id('input-text');//获取输入框的节点，输入框的值为myinput.value;
+var inputtext = myinput.value;
+var timer = null;//初始化定时器
+var nodearr = [];//定义一个空数组用来保存节点
 
 //遍历操作-广度优先／深度优先；
 //广度优先-递归;
-function mylfre(node){
+function mylfre (node){
       nodearr.push(node);
       var arr = [];
       arr.push(node);  //专门用来取第一项子节点,因为arr.shift()和arr.pop()会更改原数组，所以新建一个数组专门
@@ -33,11 +33,11 @@ function mylfre(node){
 //深度优先-递归;:children[0]==textNode,注意！所以不能用firsrElementChild,要用children[1],
 //后面要查看第一项子节点的内容所以还是从0开始。。。
 function myffre (node) {
-	if(node!==null){
+	if(node !== null){
     	nodearr.push(node);
-    	if(node.children.length>0){
-    		var child=node.children[0];
-            for(var i=0;i<node.children.length;i++){
+    	if(node.children.length > 0){
+    		var child = node.children[0];
+            for(var i = 0;i < node.children.length;i++){
                 myffre(node.children[i]);
     	    }
     	}
@@ -47,10 +47,10 @@ function myffre (node) {
 
 //创建新元素
 function createele () {
-    var ele=document.createElement('div');
-    var elep=document.createElement('p');
+    var ele = document.createElement('div');
+    var elep = document.createElement('p');
     var myinputtext;
-    if(inputtext==''){alert('请先输入内容！');}else{myinputtext=document.createTextNode(inputtext.trim());}
+    if(inputtext == ''){alert('请先输入内容！');}else{myinputtext = document.createTextNode(inputtext.trim());}
     elep.appendChild(myinputtext);
     ele.appendChild(elep);
     ele.setAttribute('class','box');
@@ -58,34 +58,39 @@ function createele () {
 }
 
 //定位元素位置
-var mynode='';
+var mynode = '';
 function divclick () {
-    document.querySelectorAll('div').forEach(function(item){
-        item.onclick = function(e){
-            if(e.target.getAttribute('class')=='box'){
+    document.querySelectorAll('div').forEach(function (item){
+        item.onclick = function (e){
+            if(e.target.getAttribute('class') == 'box'){
                  clearstyle();
                 //阻止冒泡
-                window.event ? window.event.cancleBubble==true : e.stopPropagation();
-                this.style.backgroundColor='#00f';
-                mynode=this;
+                alert(this.innerHTML);
+                e = e || window.e;
+                // var target = e.target || e.srcElement;
+                e.stopPropagation();              
+                this.style.backgroundColor = '#00f';
+                mynode = this;
             }
         }
     })
 }
 divclick();
- window.ondblclick = function(e) {
-     window.event ? window.event.cancelBubble = true : e.stopPropagation();
+ window.ondblclick = function (e) {
+     e = e || window.e;
+     // var target = e.target || e.srcElement;
+     e.stopPropagation();
      clearstyle();
 }
 //处理添加函数
 function addele () {
-   var input=$id('input-text').value.trim();
-   if(input==''){alert('请先输入内容！')}
-   else if(mynode==''){alert('请先选中节点！')}
+   var input = $id('input-text').value.trim();
+   if(input == ''){alert('请先输入内容！')}
+   else if(mynode == ''){alert('请先选中节点！')}
    else{
-       var ele=document.createElement('div');
-       var elep=document.createElement('p');
-       elep.innerHTML=input;
+       var ele = document.createElement('div');
+       var elep = document.createElement('p');
+       elep.innerHTML = input;
        ele.appendChild(elep);
        ele.setAttribute('class','box');
        mynode.appendChild(ele);
@@ -103,41 +108,41 @@ function delele () {
 
 //change_style()给遍历的节点添加额外的样式背景颜色变成黄色
 function change_style () {
-	var i=0;
-    timer=setInterval(function() {
-		if(i>nodearr.length-1){
+	var i = 0;
+    timer = setInterval(function () {
+		if(i > nodearr.length - 1){
 			clearInterval(timer);
-			nodearr[nodearr.length-1].style.backgroundColor='#0ff';
+			nodearr[nodearr.length - 1].style.backgroundColor = '#0ff';
 			return;
-		}else if(i<nodearr.length){
-		    if(i>0){
-		       nodearr[i-1].style.backgroundColor='#0ff';
+		}else if(i < nodearr.length){
+		    if(i > 0){
+		       nodearr[i - 1].style.backgroundColor = '#0ff';
 		    }
-			nodearr[i].style.backgroundColor='#ff0';
+			nodearr[i].style.backgroundColor = '#ff0';
 		}
 		i++;
 	},500);
 }
 
 //清除添加的样式
-function clearstyle(){
+function clearstyle (){
     if(nodearr.length){
         clearInterval(timer);//取消定时执行的timer,以便能随时开启另外一种遍历而不会存在上一次的timer的干扰
-        for(var i=0;i<nodearr.length;i++){
-        nodearr[i].style.backgroundColor='#fff';
+        for(var i = 0;i < nodearr.length;i++){
+        nodearr[i].style.backgroundColor = '#fff';
         }
-         nodearr=[];//清除上一次点击的样式改变
+         nodearr = [];//清除上一次点击的样式改变
     }else{
-        document.querySelectorAll("div").forEach(function(item) {
-             item.style.backgroundColor='#fff';
+        document.querySelectorAll("div").forEach(function (item) {
+             item.style.backgroundColor = '#fff';
         })
     }
 }
 
 //将各个阶段的事件写在一起方便监听
-function init(e){
+function init (e){
 	clearstyle();
-    var e=e || window.e,target=e.target || e.srcElement;
+    var e = e || window.e,target = e.target || e.srcElement;
     switch(target.id){
         case 'lf-re':
             mylfre(myroot)
@@ -151,15 +156,15 @@ function init(e){
 
 
 //各个btn的监听事件:
-window.onload=function(){
+window.onload = function (){
 	$id('lf-re').addEventListener('click',init);
 	$id('ff-re').addEventListener('click',init);
     $id('clear').addEventListener('click',clearstyle);
     
-    $id('add-ele').onclick=function(){
+    $id('add-ele').onclick = function (){
         addele();
     }
-    $id('del-ele').onclick=function(){
+    $id('del-ele').onclick = function (){
         delele();
     }
 }
