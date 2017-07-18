4d67f76cb55286f6473d9acc50db8d16fa27c01a
kerentang <1950655762@qq.com>
2017-07-18 14:24:51 +0800

    规范代码格式

--- baidu-yaoyao/ifewebz4/script.js
+++ baidu-yaoyao/ifewebz4/script.js
@@ -14,27 +14,27 @@ function drawgrid () {
     ctx.strokeStyle = '#333'
     ctx.stroke()
   }
-	    // column
-	    for (var j = 1; j < 12; j++) {
-      ctx.moveTo(40 * j, 40)
-      ctx.lineTo(40 * j, iheight - 40)
-      ctx.closePath()// 闭合路径
-      ctx.lineWidth = 1
-      ctx.strokeStyle = '#333'
-      ctx.stroke()
-	    }
-	    // num
+  // column
+  for (var j = 1; j < 12; j++) {
+    ctx.moveTo(40 * j, 40)
+    ctx.lineTo(40 * j, iheight - 40)
+    ctx.closePath()// 闭合路径
+    ctx.lineWidth = 1
+    ctx.strokeStyle = '#333'
+    ctx.stroke()
+  }
+  // num
   ctx.fillStyle = '#333'
         // row-num
-	    for (var i = 1; i < 11; i++) {
-      ctx.moveTo(20 + 40 * i, 30)
-      ctx.fillText(i, 16 + 40 * i, 30)
-	    }
-	    // column-num
-	    for (var j = 1; j < 11; j++) {
-      ctx.moveTo(20, 25 + 40 * j)
-      ctx.fillText(j, 20, 25 + 40 * j)
-	    }
+  for (var i = 1; i < 11; i++) {
+    ctx.moveTo(20 + 40 * i, 30)
+    ctx.fillText(i, 16 + 40 * i, 30)
+  }
+  // column-num
+  for (var j = 1; j < 11; j++) {
+    ctx.moveTo(20, 25 + 40 * j)
+    ctx.fillText(j, 20, 25 + 40 * j)
+  }
 }
 function $id (id) {
   return document.getElementById(id)
@@ -42,17 +42,18 @@ function $id (id) {
 var cx = document.getElementById('box')
 var cax = cx.getContext('2d')
 cax.fillStyle = '#f00'
-var bwidth = cx.width, bheight = cx.height
+var bwidth = cx.width
+var bheight = cx.height
 var raf
-	// 画个三角形，箭头朝向右边，方便第一次移动
-	// 三角形的方向由四组数据确定：左边第一位数字：0-row,1-col;第二位数字:0-left,1-right;
-	// 所以01-水平朝右；00-水平朝左；10-垂直向上；11-垂直向下
-	// （x,y）用来
+// 画个三角形，箭头朝向右边，方便第一次移动
+// 三角形的方向由四组数据确定：左边第一位数字：0-row,1-col;第二位数字:0-left,1-right;
+// 所以01-水平朝右；00-水平朝左；10-垂直向上；11-垂直向下
+// （x,y）用来
 var recangle = {
-	    x0: 20,
-	    y0: 20,
-	    vx: 40,
-	    vy: 40,
+  x0: 20,
+  y0: 20,
+  vx: 40,
+  vy: 40,
   color: '#ff0',
   draw: function () {
     cax.fillRect(this.x0 - 20, this.y0 - 20, 40, 40)
@@ -72,8 +73,8 @@ function drawrow (direct) {
   recangle.draw()
 }
 function drawcol (direct) {
-    	// cax.clearRect(0,0, bwidth, bheight);
-        // recangle.draw();
+  // cax.clearRect(0,0, bwidth, bheight);
+  // recangle.draw();
   recangle.x0 = recangle.x0
   recangle.y0 = recangle.y0 + recangle.vy * direct
   if ((recangle.y0 + recangle.vy) < 0 || (recangle.y0 + recangle.vy) > bheight + 20) {
@@ -82,42 +83,42 @@ function drawcol (direct) {
   recangle.draw()
         // raf = window.requestAnimationFrame(drawrow);
 }
-	// function turn(){
+ // function turn(){
  //        cax.clearRect(0,0,bwidth,bheight);
  //        cax.save();
 
  //        cax.restore();
-	// }
+ // }
 function init (e) {
   e = e || window.e
   var target = e.target || e.srcElement
   cax.clearRect(0, 0, bwidth, bheight)
   cax.save()
   switch (target.id) {
-        	case 'go':
-        		drawrow(1)
-        		break
-        	case 'back':
-        		cax.fillRect(this.x0 - 20, this.y0 - 20, 10, 40)
-        		drawrow(-1)
-        		break
-        	case 'up' :
-        		cax.fillRect(this.x0 - 20, this.y0 - 20, 30, 40)
-        		drawcol(-1)
-        		break
-        	case 'down' :
-        		drawcol(1)
-        		break
+    case 'go':
+      drawrow(1)
+      break
+    case 'back':
+      cax.fillRect(this.x0 - 20, this.y0 - 20, 10, 40)
+      drawrow(-1)
+      break
+    case 'up' :
+      cax.fillRect(this.x0 - 20, this.y0 - 20, 30, 40)
+      drawcol(-1)
+      break
+    case 'down' :
+      drawcol(1)
+      break
   }
   cax.restore()
 }
     // 左转
-	// 右转
-	// 向后转
+// 右转
+// 向后转
 
 function turn () {
-    	recangle.draw()
-    	cax.clearRect(0, 0, bwidth + 20, bheight + 20)
+  recangle.draw()
+  cax.clearRect(0, 0, bwidth + 20, bheight + 20)
   cax.translate(recangle.x0, recangle.y0)
   cax.save()
   cax.rotate(Math.PI)
@@ -126,7 +127,7 @@ function turn () {
 }
 
 window.onload = function () {
-    	drawgrid()
+  drawgrid()
   $id('go').addEventListener('click', init)
   $id('back').addEventListener('click', init)
   $id('up').addEventListener('click', init)
@@ -134,16 +135,16 @@ window.onload = function () {
   $id('turnback').onclick = function () {
     turn()
   }
-        // $id('turnback').onclick=function(){
-        // 	// cax.clearRect(0,0,bwidth+20,bheight+20);
-        // 	// cax.save();
-        // 	turn();
-        // 	 //cax.restore();
-        // // 	$id('go').addEventListener('click',init);
-        // // $id('back').addEventListener('click',init);
-        // }
+  // $id('turnback').onclick=function(){
+  // cax.clearRect(0,0,bwidth+20,bheight+20);
+  // cax.save();
+  // turn();
+  // cax.restore();
+  // $id('go').addEventListener('click',init);
+  // // $id('back').addEventListener('click',init);
+  // }
 }
 
  //    window.onload=function(){
-	//     document.getElementById('GO').addEventListener('click',init);
+ //      document.getElementById('GO').addEventListener('click',init);
  //    }
--- baidu-yaoyao/ifewebz7/script.js
+++ baidu-yaoyao/ifewebz7/script.js
@@ -1,178 +1,179 @@
 'use strict'
-//get dom by id
-function $id(id){
-    return document.getElementById(id);
+// get dom by id
+function $id (id) {
+  return document.getElementById(id);
 }
-//get dom by nodeName
-function $dom(mynode){
-    return document.querySelectorAll(mynode);
+// get dom by nodeName
+function $dom (mynode) {
+  return document.querySelectorAll(mynode);
 }
 // get dom
-//get input.value
-var oinput=$id('user-input');
-//get selectIndex text
+// get input.value
+var oinput = $id('user-input');
+// get selectIndex text
 var cur_index;
 
-//use obj to save
-var myobj={
-	'小明':[80,90,70],
-	'小红':[90,60,70],
-	'小亮':[80,60,90],
-	'小洲':[70,85,80],
-	'小华':[81,92,76],
-	'小敏':[66,73,89],
-	'小菲':[88,87,86]
+// use obj to save
+var myobj = {
+  '小明': [80, 90, 70],
+  '小红': [90, 60, 70],
+  '小亮': [80, 60, 90],
+  '小洲': [70, 85, 80],
+  '小华': [81, 92, 76],
+  '小敏': [66, 73, 89],
+  '小菲': [88, 87, 86]
 }
-//myobj[key]里现在只有3项数据，最后一项是前面三项之和，用一个函数来输出
+// myobj[key]里现在只有3项数据，最后一项是前面三项之和，用一个函数来输出
 // myobj[key]数组的求和函数
 // myobj[key]的值变成num
-//利用map,reduce
-function mysum(x,y){
-    return x+y;
+// 利用map,reduce
+function mysum (x, y) {
+  return x + y;
 }
-function mynum(x){
-	return x-0;
+function mynum (x) {
+  return x - 0;
 }
-function result(myobj){
-    for(var key in myobj){
-        var temparr=myobj[key].map(mynum);
-        var tempsum=temparr.reduce(mysum);
-        myobj[key].push(tempsum);
-    }
-    return myobj;
+function result (myobj) {
+  for (var key in myobj) {
+    var temparr = myobj[key].map(mynum);
+    var tempsum = temparr.reduce(mysum);
+    myobj[key].push(tempsum);
+  }
+  return myobj;
 }
-//改成检查arrofobj的length！！！
+// 改成检查arrofobj的length！！！
 // function check (myobj) {
-// 	if(result(myobj)){
-//         if(Object.keys(myobj).length){
-//             return myobj;
-//         }else{
-// 		   alert('表格中无数据请先添加！');
-// 	    }
-// 	}else{
-// 		return false;
-// 	}
+// if(result(myobj)){
+//   if(Object.keys(myobj).length){
+//      return myobj;
+//   }else{
+// alert('表格中无数据请先添加！');
 // }
-//sort up （default）
-//将对象转换成数组进行排序
-var arrofobj=[];
+// }else{
+// return false;
+// }
+// }
+// sort up （default）
+// 将对象转换成数组进行排序
+var arrofobj = [];
 result(myobj);
-function obj_to_arr(myobj){
-    for(var key in myobj){
-        var temp=myobj[key];
-        temp.unshift(key);
-        arrofobj.push(temp);
-    }
-    return arrofobj;
+function obj_to_arr (myobj) {
+  for (var key in myobj) {
+    var temp = myobj[key];
+    temp.unshift(key);
+    arrofobj.push(temp);
+  }
+  return arrofobj;
 }
 obj_to_arr(myobj);
-function sort_up(arrofobj){
-	cur_index=$id('sortby').selectedIndex;
-    if(arrofobj.length){
-        arrofobj.sort(function(a,b){
-            return a[cur_index+1]-b[cur_index+1];
-    	});      
-    }else{
-    	alert('表格已空请先添加！');
-    }
-}
-//sort down
-function sort_down (arrofobj) {
-	cur_index=$id('sortby').selectedIndex;
-	if(arrofobj.length){
-    	arrofobj.sort(function(a,b){
-            return b[cur_index+1]-a[cur_index+1];
+function sort_up (arrofobj) {
+  cur_index = $id('sortby').selectedIndex;
+  if (arrofobj.length) {
+    arrofobj.sort(function (a, b) {
+      return a[cur_index + 1] - b[cur_index + 1];
     	});
-    }else{
+  } else{
     	alert('表格已空请先添加！');
-    }
+  }
+}
+// sort down
+function sort_down (arrofobj) {
+  cur_index = $id('sortby').selectedIndex;
+  if (arrofobj.length) {
+    	arrofobj.sort(function (a, b) {
+      return b[cur_index + 1] - a[cur_index + 1];
+    	});
+  }else {
+    	alert('表格已空请先添加！');
+  }
 }
 
-//delete
+// delete
 function delerow (arrofobj) {
-	// 删除之前先检查一下table的children
-	if(arrofobj.length==0){
-    	alert('表格已空请先添加！');
-    	return false;
-    }else{
-    	arrofobj.pop();
-    	return arrofobj;
-    }
+  // 删除之前先检查一下table的children
+  if (arrofobj.length == 0) {
+    alert('表格已空请先添加！');
+    return false;
+  } else {
+    arrofobj.pop();
+    return arrofobj;
+  }
 }
-//insert one line
+// insert one line
 // var strarr;
 // function checkinput (oinput) {
-// 	var str=oinput.value.trim();
-// 	if(str.length!==4){
-// 		alert('输入长度不正确！请重新输入！')
+// var str=oinput.value.trim();
+// if(str.length!==4){
+// alert('输入长度不正确！请重新输入！')
 //        return false;
-// 	}else{
-// 		strarr=str.split(/[,， ；]/);
-// 		for(var j=1;j<strarr.length;j++){
+// } else{
+// strarr=str.split(/[,， ；]/);
+// for(var j=1;j<strarr.length;j++){
 //             if(strarr[j]!=='' && !isNaN(strarr[j])){
 //                 myobj[strarr[0]]=strarr.slice(1);
 //             }
-// 		}
+// }
 //         return myobj;
-// 	}
 // }
-//获取用户自己输入一段数据生成到表格中
+// }
+// 获取用户自己输入一段数据生成到表格中
 // function user_input(myobj){
-// 	if(checkinput (oinput)){
+// if (checkinput (oinput)){
 //         result(myobj);
-//         return 	obj_to_arr(myobj);
-// 	}else{
-// 		return myobj;
-// 	}
-        	
+//        return obj_to_arr(myobj);
+// } else{
+// 	return myobj;
 // }
-//render to dom：1.createElement;2.appendChild()
-function createtd(val){
-	var otd=document.createElement('td');
-	 otd.innerText=val;
-	 return otd;
+
+// }
+// render to dom：1.createElement;2.appendChild()
+function createtd (val) {
+  var otd = document.createElement('td');
+  otd.innerText = val;
+  return otd;
 }
-function createtr(){
-	var otr=document.createElement('tr');
-	return otr;
+function createtr () {
+  var otr = document.createElement('tr');
+  return otr;
 }
-function createtable(arrofobj) {
-	//td,tr.appendChild(td),td.innerHTML=myobj[i]
-	var mytbl=$id('tbl');
-    mytbl.innerHTML='<tr><th>姓名</th><th>语文</th><th>数学</th><th>英语</th><th>总分</th></tr>';
-	for(var l=0;l<arrofobj.length;l++){
-        var trs=createtr();
-		for(var n=0;n<arrofobj[l].length;n++){
-           var tds=createtd(arrofobj[l][n]);
-            trs.appendChild(tds);
-		}
-        mytbl.appendChild(trs);		
-	}
+function createtable (arrofobj) {
+  // td,tr.appendChild(td),td.innerHTML=myobj[i]
+  var mytbl = $id('tbl');
+  mytbl.innerHTML = '<tr><th>姓名</th><th>语文</th><th>数学</th><th>英语</th><th>总分</th></tr>';
+  for (var l = 0; l < arrofobj.length; l++) {
+    var trs = createtr();
+    for (var n = 0; n < arrofobj[l].length; n++) {
+      var tds = createtd(arrofobj[l][n]);
+      trs.appendChild(tds);
+    }
+    mytbl.appendChild(trs);
+  }
 }
 
- function render(){
-    //首先重置table.children;
-    $id('tbl').innerHTML='';
-    
-    createtable(arrofobj);
- }
-//统一处理函数-代理
-function init(e){
-	var e=e || window.e ,target=e.target || e.srcElement;
-	switch(target.id){
-        case 'up':
-        	sort_up(arrofobj)
-        	break;
-        case 'down':
-        	sort_down (arrofobj)
-        	break;
-        case 'dele':
-        	delerow (arrofobj)
-        	break;
-	}
-    render();
+function render () {
+    // 首先重置table.children;
+  $id('tbl').innerHTML = '';
+
+  createtable(arrofobj);
 }
-//addEventListener
-window.onload=function(){
-	$id('mybtn').addEventListener('click',init);
+// 统一处理函数-代理
+function init (e) {
+  e = e || window.e
+  let target = e.target || e.srcElement;
+  switch (target.id) {
+    case 'up':
+      sort_up(arrofobj)
+      break;
+    case 'down':
+      sort_down(arrofobj)
+      break;
+    case 'dele':
+      delerow(arrofobj)
+      break;
+  }
+  render();
+}
+// addEventListener
+window.onload = function () {
+  $id('mybtn').addEventListener('click', init);
 }
